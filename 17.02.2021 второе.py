2ca75de99c914b53c74fede7af302d56bf931daf
pin034 <60174760+pin034@users.noreply.github.com>
2021-02-17 22:58:50 +0700

    Create 17.02.2021 второе.py

---
+++ 17.02.2021 второе.py
@@ -0,0 +1,7 @@
+a,d,c = map(int,input("Введите три числа:\n").split(" "))
+s =a + d + c
+m = a * d *c
+cr = (a+d+c)/3
+print(a,"+",d,"+",c,"=",s)
+print(a,"*",d,"*",c,"=",m)
+print("(",a,"*",d,"*",c,")/3 =",cr)
