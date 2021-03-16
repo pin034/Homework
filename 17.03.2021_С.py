6da7cf66cdda20a6064d44ca533757ef74da115d
pin034 <60174760+pin034@users.noreply.github.com>
2021-03-16 21:41:45 +0700

    Create 17.03.2021_С.py

---
+++ 17.03.2021_С.py
@@ -0,0 +1,9 @@
+from random import randint
+a = randint (1,6)
+b = randint (1,6)
+c = randint (1,6)
+ch = a*100 + b*10 + c*1
+k = ch**2
+print("Выпало очков:\t",a,b,c)
+print("Число",ch)
+print("Его квадрат",k)
